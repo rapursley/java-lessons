456f99e15be20d8c589e1f7c75d149319d8db81b
rapursley <rpursley@gmail.com>
2017-11-04 19:01:46 -0500

    lesson 11

--- demo/src/demo/MyLinkedList.java
+++ demo/src/demo/MyLinkedList.java
@@ -1,47 +1,79 @@
 package demo;
 
-public class MyLinkedList<T> {
+public class MyLinkedList<T extends Comparable<T>> {
 
-		private Node<T> head;
-		private Node<T> tail;
-		
-		public MyLinkedList() {
-			super();
-			head = null;
-			tail = null;
+	private Node<T> head;
+	private Node<T> tail;
+
+	public MyLinkedList() {
+		super();
+		head = null;
+		tail = null;
+	}
+
+	public void addAtTail(T data) {
+		Node<T> node = new Node<>(data);
+
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			// adding to the end
+			node.setPrevious(tail);
+			tail.setNext(node);
+			tail = node;
 		}
-		
-		public void add(T data){
-			Node<T> node = new Node<>(data);
-			
-			if(head == null){
-				head = node;
-				tail = node;
-			} else {
-				//adding to the end
-				node.setPrevious(tail);
-				tail.setNext(node);
-				tail = node;
-			}
+	}
+
+	public void addInOrder(T data) {
+		Node<T> after = head;
+		Node<T> before = null;
+
+		while (after != null && data.compareTo(after.getData()) > 0) {
+			before = after;
+			after = after.getNext();
 		}
-		
-		@Override
-		public String toString(){
-			//if(head == null) return "null";
-			String s = "head ==> ";
-			Node<T> current = head;
-			while (current != null) {
-				s += current.getData() + " ==> ";
-				current = current.getNext();
-			}
-			s += "null";
-			s += "\ntail ==> ";
-			current = tail;
-			while (current != null) {
-				s += current.getData() + " ==> ";
-				current = current.getPrevious();
-			}
-			s += "null";
-			return s;
+
+		if (after == null) {
+			addAtTail(data);
+			return;
 		}
+
+		Node<T> node = new Node<>(data);
+
+		if (before == null) {
+			head = node;
+			// adding between before and after
+			node.setPrevious(null);
+			node.setNext(after);
+			after.setPrevious(node);
+		} else {
+			// adding between before and after
+			node.setPrevious(after.getPrevious());
+			node.setNext(before.getNext());
+			before.setNext(node);
+			after.setPrevious(node);
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		// if(head == null) return "null";
+		String s = "head ==> ";
+		Node<T> current = head;
+		while (current != null) {
+			s += current.getData() + " ==> ";
+			current = current.getNext();
+		}
+		s += "null\n";
+		String s2 = " <== tail";
+		current = tail;
+		while (current != null) {
+			s2 = " <== " + current.getData() + s2;
+			current = current.getPrevious();
+		}
+		s2 = "null" + s2;
+		return s + s2;
+	}
 }
--- demo/src/demo/TestLinkedList.java
+++ demo/src/demo/TestLinkedList.java
@@ -3,16 +3,47 @@ package demo;
 public class TestLinkedList {
 
 	public static void main(String[] args) {
-		MyLinkedList<String> list = new MyLinkedList<>();
+		System.out.println("Testing MyLinkedList with String objects.");
 		
-		list.add("a");
-		list.add("b");
-		list.add("c");
-		list.add("d");
-		list.add("e");
-		list.add("f");
+		MyLinkedList<String> list1 = new MyLinkedList<>();
+		
+		list1.addAtTail("a");
+		list1.addAtTail("b");
+		list1.addAtTail("c");
+		list1.addAtTail("d");
+		list1.addAtTail("e");
+		list1.addAtTail("f");
+		list1.addAtTail("g");
 	
-		System.out.println(list);
+		System.out.println(list1);
+		
+		System.out.println("Testing MyLinkedList with Integer objects.");
+		
+		MyLinkedList<Integer> list2 = new MyLinkedList<>();
+		
+		list2.addAtTail(1);
+		list2.addAtTail(2);
+		list2.addAtTail(3);
+		list2.addAtTail(4);
+		list2.addAtTail(5);
+		list2.addAtTail(6);
+		list2.addAtTail(7);
+	
+		System.out.println(list2);
+		
+		System.out.println("Testing MyLinkedList with adding Integer objects in order.");
+		
+		MyLinkedList<Integer> list3 = new MyLinkedList<>();
+		
+		list3.addInOrder(2);
+		list3.addInOrder(4);
+		list3.addInOrder(6);
+		list3.addInOrder(9);
+		list3.addInOrder(3);
+		list3.addInOrder(5);
+		list3.addInOrder(7);
+	
+		System.out.println(list3);
 	}
 
 }
